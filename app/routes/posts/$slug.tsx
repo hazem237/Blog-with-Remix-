3d691f415e24146964fb20eeed8b7f3fcccbddc1
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-01-15 19:00:10 -0800

    Added types to dynamic routes  for powerful performance

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,27 +1,32 @@
 import {marked} from "marked"
 import { json, LoaderFunction, useLoaderData } from "react-router"
 import { Link } from "react-router-dom"
-import { getPostBasedSlug } from "~/model/posts.server"
+import getPosts, { getPostBasedSlug } from "~/model/posts.server"
+import invariant from "tiny-invariant" 
 
+type LoaderDataType ={
+    title:string
+    html : string
+}
 export const loader:LoaderFunction=async({params})=>{
   const {slug} = params 
+  invariant(slug , "slug is required !")
   const post = await getPostBasedSlug(slug)
+  invariant(post , `post is required ${slug}`)
   const html = marked(post.markdown)
-  return json({post , html})
+  return json<LoaderDataType>({title:post.title , html})
 }
 
 export default function RoutesPost () {
-    const {post , html} = useLoaderData();
+    const {title , html} = useLoaderData() as LoaderDataType;
     return (
         <main>
-            <div>{post.title}</div>
+            <div>{title}</div>
             <div dangerouslySetInnerHTML={{__html:html}}/>
-            <div>{post.createdAt}</div>
             <Link to={''}>
                 Home
             </Link>
         </main>
     )
 }
-
 
