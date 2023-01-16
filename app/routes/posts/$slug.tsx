ae828c10f6e2b6ec3f83b1426d2cbebce169d53d
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-01-15 18:48:11 -0800

    Added marked library for better html showing content

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,17 +1,27 @@
+import {marked} from "marked"
 import { json, LoaderFunction, useLoaderData } from "react-router"
+import { Link } from "react-router-dom"
 import { getPostBasedSlug } from "~/model/posts.server"
 
 export const loader:LoaderFunction=async({params})=>{
   const {slug} = params 
   const post = await getPostBasedSlug(slug)
-  return json({post})
+  const html = marked(post.markdown)
+  return json({post , html})
 }
 
 export default function RoutesPost () {
-    const {post} = useLoaderData();
+    const {post , html} = useLoaderData();
     return (
         <main>
             <div>{post.title}</div>
+            <div dangerouslySetInnerHTML={{__html:html}}/>
+            <div>{post.createdAt}</div>
+            <Link to={''}>
+                Home
+            </Link>
         </main>
     )
 }
+
+
